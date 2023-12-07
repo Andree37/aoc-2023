f8c0c2ed99eeabce359b072ad8a20960a2ebae35
Andre Ribeiro <andresmribeiro@gmail.com>
2023-12-07 15:54:20 +0000

    day 4 part 2

--- day04/main.cpp
+++ day04/main.cpp
@@ -2,6 +2,7 @@
 #include <sstream>
 #include <string>
 #include <cmath>
+#include <map>
 
 
 void part1()
@@ -54,7 +55,64 @@ Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11)");
     std::cout << total << std::endl;
 }
 
+void part2()
+{
+    std::istringstream input(
+        R"(Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53
+Card 2: 13 32 20 16 61 | 61 30 68 82 17 32 24 19
+Card 3:  1 21 53 59 44 | 69 82 63 72 16 21 14  1
+Card 4: 41 92 73 84 69 | 59 84 76 51 58  5 54 83
+Card 5: 87 83 26 28 32 | 88 30 70 12 93 22 82 36
+Card 6: 31 18 13 56 72 | 74 77 10 23 35 67 36 11)");
+
+    std::string line;
+    int total = 0;
+    std::map<int, int> scratchcards;
+    while (getline(input, line, '\n'))
+    {
+        std::string token;
+
+        std::stringstream line_stream(line);
+        line_stream >> token;
+        line_stream >> token;
+        int card_no = atoi(token.c_str());
+
+        scratchcards[card_no] += 1;
+        total += scratchcards[card_no];
+
+        std::vector<std::string> card;
+
+        while (line_stream >> token)
+        {
+            if (token == "|")
+            {
+                break;
+            }
+
+            card.push_back(token);
+        }
+
+        int ic = 0;
+        while (line_stream >> token)
+        {
+            for (const auto& i : card)
+            {
+                if (i == token)
+                {
+                    scratchcards[card_no + ++ic] += 1 * scratchcards[card_no];
+                }
+            }
+        }
+    }
+    for (auto sc : scratchcards)
+    {
+        std::cout << sc.first << " " << sc.second << std::endl;
+    }
+    std::cout << total << std::endl;
+}
+
 int main()
 {
-    part1();
+    //part1();
+    part2();
 }
